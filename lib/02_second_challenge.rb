0534107591bee758ed1f09cc3b65bcd8249ad854
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-08 01:06:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,7 +3,8 @@ def second_challenge
     :montague => {
       :patriarch => {},
       :matriarch => {},
-      :hero =>
+      :hero => {},
+      :hero
       }, 
     :capulet => {
       #your key/value pairs here!
